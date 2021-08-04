88be45fcb5ac0b503cd8cae51e8ddc5ccd6a2222
Sergio Martinez <sergio.iyo08@gmail.com>
2021-08-04 18:05:15 -0500

    docs agregado

---
+++ docs/precache-manifest.1ec0b09152ac567236f19d0bffb523cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae129ea09275647d3c70a0cb42c951ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40f28c8f5f78376a3982",
+    "url": "/static/css/main.7b9a6aec.chunk.css"
+  },
+  {
+    "revision": "e107496e2e01402b74e7",
+    "url": "/static/js/2.b11609f6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.b11609f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40f28c8f5f78376a3982",
+    "url": "/static/js/main.fee4e3b9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
